e6f2fbf6d6868a43af653e167030fc6853eab4e6
Masum Billal <mbillal.eee@gmail.com>
2023-07-27 14:32:06 +0600

    Admin Logout Option--13

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class AdminController extends Controller
 {
@@ -11,4 +14,17 @@ class AdminController extends Controller
     {
         return view('admin.index');
     }
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+
+    
 }
